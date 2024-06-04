94de2e8c48cb0f0d265f64c5c9e95dfbe69abc70
Angelo-Mansabdar <153824794+Angelo-Mansabdar@users.noreply.github.com>
2024-06-04 11:50:05 +0200

    test

--- dbcalls/users.php
+++ dbcalls/users.php
@@ -1,57 +1,29 @@
 <?php
 include('connect.php');
 
+if ( $_SERVER['REQUEST_METHOD'] == 'POST' )
+{
+    if(isset($_POST['create_user']))
+    {
+        $is_admin = isset($_POST['is_admin']) ? 1 : 0;
+        $user_name = isset($_POST['user_name']);
+        $user_lastname = isset($_POST['user_lastname']);
+        $user_pass = isset($_POST['user_pass']);
+        $user_email = isset($_POST['user_email']);
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if (isset($_POST['create_user'])) {
-        $is_admin = isset($_POST['is_admin']) ? 1 : 0;
-        $user_name = $_POST['user_name'];
-        $user_lastname = $_POST['user_lastname'];
-        $user_pass = ($_POST['user_pass']);
-        $user_email = $_POST['user_email'];
-        
-        $sql = "INSERT INTO users (is_admin, user_name, user_lastname, user_pass, user_email) 
-                VALUES (:is_admin, :user_name, :user_lastname, :user_pass, :user_email)";
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute([
-            ':is_admin' => $is_admin,
-            ':user_name' => $user_name,
-            ':user_lastname' => $user_lastname,
-            ':user_pass' => $user_pass,
-            ':user_email' => $user_email
-        ]);
-    } elseif (isset($_POST['update_user'])) {
-        $user_id = $_POST['user_id'];
-        $is_admin = isset($_POST['is_admin']) ? 1 : 0;
-        $user_name = $_POST['user_name'];
-        $user_lastname = $_POST['user_lastname'];
-        $user_pass =($_POST['user_pass']);
-        $user_email = $_POST['user_email'];
-        
-        $sql = "UPDATE users SET 
-                is_admin = :is_admin, 
-                user_name = :user_name, 
-                user_lastname = :user_lastname, 
-                user_pass = :user_pass, 
-                user_email = :user_email 
-                WHERE user_id = :user_id";
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute([
-            ':is_admin' => $is_admin,
-            ':user_name' => $user_name,
-            ':user_lastname' => $user_lastname,
-            ':user_pass' => $user_pass,
-            ':user_email' => $user_email,
-            ':user_id' => $user_id
-        ]);
-    } elseif (isset($_POST['delete_user'])) {
-        $user_id = $_POST['user_id'];
-        
-        $sql = "DELETE FROM users WHERE user_id = :user_id";
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute([':user_id' => $user_id]);
+        $stmt = $conn->PREPARE("INSERT INTO users( is_admin, user_name, user_lastname, user_pass, user_email )
+                                VALUES ( :is_admin, :user_name :user_lastname, :user_pass, :user_email )");
+
+        $stmt->bindparam(':is_admin', $is_admin );
+        $stmt->bindparam(':user_name', $user_name );
+        $stmt->bindparam(':user_lastname', $user_lastname);
+        $stmt->bindparam(':user_pass', $user_pass);
+        $stmt->bindparam(':user_email', $user_email);
+        $stmt->execute();
+
+
     }
-}
 
-$stmt = $pdo->query("SELECT * FROM users");
-$allUsers = $stmt->fetchAll();
+
+
+}
--- header.php
+++ header.php
@@ -46,6 +46,37 @@ session_start();
 <body>
 
 <header>
+<div class="buttomheaderresponsive" style="background-image: url('assets/img/header_img.png');">
+        <div class="logoheader">
+            <a href="index.php"><img src="assets/img/logo.png" alt="logo" id="logoheader"></a>
+        </div>
+        <div id="myNav" class="overlay">
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()"
+            >&times;</a
+            >
+            <div class="overlay-content">
+                <a href="voorgesteldereizen.php">trip info</a>
+                <a href="contact.php">contact</a>
+                <a href="test.php">About us</a>
+            </div>
+        </div>
+        <span
+                id="bar"
+                style="font-size: 30px; cursor: pointer"
+                onclick="openNav()"
+        >&#9776;
+    </span>
+
+        <script>
+            function openNav() {
+                document.getElementById("myNav").style.width = "100%";
+
+
+function closeNav() {
+                document.getElementById("myNav").style.width = "0%";
+            }
+        </script>
+    </div>
     <div class="topheader"></div>
     <div class="buttomheader" style="background-image: url('assets/img/header_img.png');">
         <div class="logoheader">
